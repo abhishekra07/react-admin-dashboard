f1a8d02974efcc0c9f5d4c91ffaf916454340627
abhishekra07 <40576705+abhishekra07@users.noreply.github.com>
2021-10-03 22:24:24 +0530

    added sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import TopBar from './components/topbar/TopBar';
+import Sidebar from './components/sidebar/Sidebar';
+import './app.css';
 
 function App() {
   return (
     <div className="App">
       <TopBar />
+      <div className="container">
+        <Sidebar />
+      </div>
     </div>
   );
 }
