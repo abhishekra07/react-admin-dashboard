adac86493fc1088703da60600dac702b5c83455d
abhishekra07 <40576705+abhishekra07@users.noreply.github.com>
2021-10-04 21:28:42 +0530

    added large widget component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo'
 import Chart from "../../components/chart/Chart";
 import WidgetSm from "../../components/widgetSm/WidgetSm";
+import WidgetLg from "../../components/widgetLg/WidgetLg";
 import './home.css';
 
 import { userData } from "../../dummyData";
@@ -12,6 +13,7 @@ function Home() {
       <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
         <div className="homeWidgets">
           <WidgetSm/>
+          <WidgetLg />
         </div>
     </div>
   )
