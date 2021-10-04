1dbddedaba2ab80989a25fc55ef065489810bddd
abhishekra07 <40576705+abhishekra07@users.noreply.github.com>
2021-10-04 21:20:00 +0530

    added chart component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,14 @@
-import FeaturedInfo from './../../components/featuredInfo/FeaturedInfo'
-
+import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo'
+import Chart from "../../components/chart/Chart";
 import './home.css';
+
+import { userData } from "../../dummyData";
 
 function Home() {
   return(
     <div className='home'>
       <FeaturedInfo />
+      <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
     </div>
   )
 }
